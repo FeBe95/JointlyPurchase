79bec3b46ffe8d64031656c51ae75a4be2421f9e
paterlewis <brummbrumm1995@googlemail.com>
2014-06-27 13:39:24 +0200

    Erfahre mehr update

--- Pages/ErfahreMehr.php
+++ Pages/ErfahreMehr.php
@@ -18,28 +18,30 @@
         <?php 
             include "../Templates/LoginHeadTemplate.php"
         ?>
-		<section>
-			<div class="learnMoreTitelBox" id="first">
-				<h2>Das ist unser Ziel</h2>
-				<p>BlaBlaBla BlaBlaBla BlaBlaBla</p>
-				<p>Noch mehr BlaBlaBla BlaBlaBla BlaBlaBla</p>
-				<p>Nicht zu vergessen ist BlaBlaBla BlaBlaBla BlaBlaBla</p>
+		<div id="container">
+			<div id="section1">
+				<div id="content1">
+					<div class="learnMoreTitelBox" id="first">
+						<h2>Das ist unser Ziel</h2>
+						<p>BlaBlaBla BlaBlaBla BlaBlaBla</p>
+						<p>Noch mehr BlaBlaBla BlaBlaBla BlaBlaBla</p>
+						<p>Nicht zu vergessen ist BlaBlaBla BlaBlaBla BlaBlaBla</p>
+					</div>
+					<img style="width:400px;display:inline;" src="../Pictures/SiteContent/Spokesman_Character_Big.png">
+				</div>
 			</div>
-			<div class="Picture1">
-				<img style="width:600px;" src="../Pictures/SiteContent/Spokesman_Character_Big.png">
+			<div id="section2" style="background-color:#B3C833;">
+				<div id="content2">
+						<img style="width:400px;display:inline;" src="../Pictures/SiteContent/Caveman_Character.png">
+					<div class="learnMoreTitelBox" id="second">
+						<h2>Das ist unser Ziel</h2>
+						<p>BlaBlaBla BlaBlaBla BlaBlaBla</p>
+						<p>Noch mehr BlaBlaBla BlaBlaBla BlaBlaBla</p>
+						<p>Nicht zu vergessen ist BlaBlaBla BlaBlaBla BlaBlaBla</p>
+					</div>
+				</div>
 			</div>
-		</section>
-		<section style="background-color:#B3C833;">
-			<div class="Picture2">
-				<img style="width:600px;" src="../Pictures/SiteContent/Caveman_Character.png">
-			</div>
-			<div class="learnMoreTitelBox" id="second">
-				<h2>Das ist unser Ziel</h2>
-				<p>BlaBlaBla BlaBlaBla BlaBlaBla</p>
-				<p>Noch mehr BlaBlaBla BlaBlaBla BlaBlaBla</p>
-				<p>Nicht zu vergessen ist BlaBlaBla BlaBlaBla BlaBlaBla</p>
-			</div>
-		</section>
+		</div>
 		<footer id="site-footer">
 			<div id="wrap">  
 				<p id="site-credits">Made with <span>&hearts;</span> in Burgholzhausen, DE. Copyright 2014</p>

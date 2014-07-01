6625b2360d4611774bbb8a4bc7dd9d0822867106
paterlewis <brummbrumm1995@googlemail.com>
2014-07-01 18:46:37 +0200

    mini fix FriendBrowser/ man sieht wieder die Namen

--- Php/SocialNetwork/FriendBrowser.php
+++ Php/SocialNetwork/FriendBrowser.php
@@ -5,13 +5,13 @@ $res = mysqli_query($conUser,"SELECT * FROM user where plz = ".$dsatz['plz']." a
 $num = mysqli_num_rows($res);
 echo"<div id='right'>";
 // Tabellenbeginn
-echo "<table cellspacing='10px' style='float:right;' >";
+echo "<table cellspacing='10px'>";
 $i= 1;
 while (($dsatz = mysqli_fetch_assoc($res)) && ($i <= 5))
 {
-$name= $dsatz["vorname"]." ".$dsatz["name"];
+$name=  $dsatz["vorname"]." ".$dsatz["name"];
 echo "<tr>";
-echo "<td class='friend_block'><a class='friend_link' href='../Pages/Profil.php?a=".$dsatz['ID']."'><div id='profilPicCrop'><img id='profilcrop' src='../Pictures/Thumbnails/".$dsatz["profilPic"]."'>".$name."</div></a></td>";
+echo "<td class='friend_block'><a class='friend_link' href='../Pages/Profil.php?a=".$dsatz['ID']."'><div id='profilPicCrop'><img id='profilcrop' src='../Pictures/Thumbnails/".$dsatz["profilPic"]."'><span style='vertical-align:top;margin-left:10px;'>".$name."</span></div></a></td>";
 echo "</tr>";
 $i ++;
 }

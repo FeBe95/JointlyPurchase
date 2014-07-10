649970c9139d584999226767314cb6db9aa94eb7
paterlewis <brummbrumm1995@googlemail.com>
2014-07-10 18:33:08 +0200

    Revert "Revert ":+1:""
    This reverts commit 4e797cf8d9ff3817c076f3f074d56aaf87e204b8.

--- Php/Shoppinglist/AddShoppinglist.php
+++ Php/Shoppinglist/AddShoppinglist.php
@@ -11,17 +11,38 @@
 	$datum = date("d.m.Y",$timestamp);
 	$uhrzeit = date("H:i",$timestamp);
 	$date = "".$datum." - ".$uhrzeit."";
+	$stmt = mysqli_stmt_init($conUser);
+	$i = 0;
+	If (mysqli_stmt_prepare($stmt, 'SELECT * FROM einkaufslisten WHERE listName = (?) && userID = (?)')){
+		mysqli_stmt_bind_param($stmt,'si', $_POST['list'], $ID);
+		mysqli_stmt_execute($stmt);
+		while (mysqli_stmt_fetch($stmt)){
+		$i++;
+		}
+	}
+	mysqli_stmt_close($stmt);
 
-	$abf1 = mysqli_query($conUser,"SELECT * FROM einkaufslisten WHERE listName = '".$table."'&& userID = ".$ID."");
-	$abf4 = "INSERT einkaufslisten (userID,listName,listID) values ('".$ID."','".$table."','".$listID."')";
+	//$abf1 = mysqli_query($conUser,"SELECT * FROM einkaufslisten WHERE listName = '".$table."'&& userID = ".$ID."");
+	//$abf4 = "INSERT einkaufslisten (userID,listName,listID) values ('".$ID."','".$table."','".$listID."')";
 	//$abf6 = "SELECT listID FROM einkaufslisten WHERE userID = ".$ID."";
 	$abf8 = "UPDATE einkaufslisten SET date='".$date."' WHERE listID ='".$listID."'";
 	
-	if(mysqli_num_rows($abf1)== 0 && $_POST['list'] != ""){
+	if($i == 0 && $_POST['list'] != ""){
 		if (!(empty($_POST["list"]))){
 			$_SESSION["list"] = $_POST["list"];
-			mysqli_query($conUser,$abf4);
-			mysqli_query($conUser,$abf8);
+			$stmt = mysqli_stmt_init($conUser);
+			If (mysqli_stmt_prepare($stmt, 'INSERT einkaufslisten (userID,listName,listID) values ((?),(?),(?))')){
+				mysqli_stmt_bind_param($stmt,'iss', $ID, $_POST['list'], md5($table.$ID) );
+				mysqli_stmt_execute($stmt);
+			}
+			mysqli_stmt_close($stmt);
+			$stmt = mysqli_stmt_init($conUser);
+			If (mysqli_stmt_prepare($stmt, 'UPDATE einkaufslisten SET date=(?) WHERE listID =(?)')){
+				mysqli_stmt_bind_param($stmt,'ii', $date, md5($table.$ID));
+				mysqli_stmt_execute($stmt);
+			}
+			mysqli_stmt_close($stmt);
+			//mysqli_query($conUser,$abf8);
 		}
 		header( 'Location: ../../Pages/ShoppinglistEditor.php' ) ; 
 	}

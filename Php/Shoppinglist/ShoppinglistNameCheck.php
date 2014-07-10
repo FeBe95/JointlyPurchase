fde7e6d7d2cacea89621c8e27a0286282a9761ce
paterlewis <brummbrumm1995@googlemail.com>
2014-07-10 18:28:59 +0200

    :+1:

--- Php/Shoppinglist/ShoppinglistNameCheck.php
+++ Php/Shoppinglist/ShoppinglistNameCheck.php
@@ -1,19 +1,19 @@
-		<?php
-			session_start();
-			include "../Authentification/SessionChecker.php";
-			include "../Php/Misc/GetYourData.php";
-			include"../../Templates/MYSQLConnectionString.php";
-			$table = $_POST['list'] ;
-			$abf1 = mysqli_query($conShoppinglist,"select * from List_".$table."_".$ID."");
-			echo $ID;
-			if($abf1==false)
-			{
-				if (!(empty($_POST["list"]))){
-					$_SESSION["list"] = $_POST["list"];
-					}
-				header( 'Location: ../../Pages/ShoppinglistEditor.php' ) ;
-			}else
-			{
-				echo "Du hast bereits eine Einkaufsliste mit diesem Namen bitte wähle einen anderen.";
-			}
-		?>
+		// <?php
+			// session_start();
+			// include "../Authentification/SessionChecker.php";
+			// include "../Php/Misc/GetYourData.php";
+			// include"../../Templates/MYSQLConnectionString.php";
+			// $table = $_POST['list'] ;
+			// $abf1 = mysqli_query($conShoppinglist,"select * from List_".$table."_".$ID."");
+			// echo $ID;
+			// if($abf1==false)
+			// {
+				// if (!(empty($_POST["list"]))){
+					// $_SESSION["list"] = $_POST["list"];
+					// }
+				// header( 'Location: ../../Pages/ShoppinglistEditor.php' ) ;
+			// }else
+			// {
+				// echo "Du hast bereits eine Einkaufsliste mit diesem Namen bitte wähle einen anderen.";
+			// }
+		// ?>
